18acf18d2963ad4ab5c1b1fb4e90bd65f6cf3bf1
bevanger <baileyevanger@gmail.com>
2021-09-08 19:59:55 -0600

    finished writting models

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -150,12 +150,21 @@ function findSteps(scheme_id) { // EXERCISE C
 }
 
 function add(scheme) { // EXERCISE D
+  return db('schemes').insert(scheme)
+    .then((scheme_id) => {
+      return db('schemes').where('scheme_id', scheme_id).first()
+    })
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
 }
 
 function addStep(scheme_id, step) { // EXERCISE E
+  step.scheme_id = scheme_id;
+  return db('steps').insert(step)
+    .then((step_id) => {
+      return db('steps').where('scheme_id', scheme_id).orderBy('step_number', 'asc')
+    })
   /*
     1E- This function adds a step to the scheme with the given `scheme_id`
     and resolves to _all the steps_ belonging to the given `scheme_id`,
